3994c46b7e350dd6830e730fb6ad7905cfa0600e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-01-26 15:32:45 -0500

    started work on gravisuit flight

--- src/main/java/trinsdar/gravisuit/items/ItemArmorGravisuit.java
+++ src/main/java/trinsdar/gravisuit/items/ItemArmorGravisuit.java
@@ -6,11 +6,18 @@ import ic2.core.item.armor.base.ItemArmorElectricJetpackBase;
 import ic2.core.item.armor.base.ItemArmorJetpackBase;
 import ic2.core.item.armor.base.ItemArmorJetpackBase.IIndirectJetpack;
 import ic2.core.item.armor.electric.ItemArmorQuantumSuit;
+import ic2.core.platform.lang.storage.Ic2InfoLang;
+import ic2.core.platform.player.PlayerHandler;
+import ic2.core.util.misc.StackUtil;
 import ic2.core.util.obj.ToolTipType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -139,6 +146,68 @@ public class ItemArmorGravisuit extends ItemArmorQuantumSuit implements IIndirec
         @Override
         public double getTransferLimit(ItemStack itemStack) {
             return 0;
+        }
+
+        @Override
+        public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
+            NBTTagCompound nbt = StackUtil.getOrCreateNbtData(stack);
+            boolean disabled = nbt.getBoolean("disabled");
+            PlayerHandler handler = PlayerHandler.getHandlerForPlayer(player);
+            byte jetpackTicker = nbt.getByte("JetpackTicker");
+            Entity entity = player.getLowestRidingEntity();
+            boolean server = IC2.platform.isSimulating();
+            if (disabled) {
+                if (server) {
+                    if (jetpackTicker > 0) {
+                        --jetpackTicker;
+                        nbt.setByte("JetpackTicker", jetpackTicker);
+                    } else if (handler.toggleKeyDown) {
+                        nbt.setByte("JetpackTicker", (byte)10);
+                        nbt.setBoolean("disabled", false);
+                        IC2.platform.messagePlayer(player, Ic2InfoLang.jetpackOn);
+                    }
+                }
+
+            } else {
+                if (handler.toggleKeyDown && jetpackTicker <= 0) {
+                    if (server) {
+                        nbt.setBoolean("disabled", true);
+                        nbt.setByte("JetpackTicker", (byte)10);
+                        IC2.platform.messagePlayer(player, Ic2InfoLang.jetpackOff);
+                        return;
+                    }
+                } else if (jetpackTicker > 0) {
+                    --jetpackTicker;
+                    nbt.setByte("JetpackTicker", jetpackTicker);
+                }
+            }
+
+            if (!disabled && ElectricItem.manager.getCharge(stack) >= 512 && player.hasItemInSlot(EntityEquipmentSlot.CHEST)){
+                this.useEnergy(player, stack, 512);
+                player.capabilities.allowFlying = true;
+                player.stepHeight = 1.0625F;
+                boolean flying = player.capabilities.isFlying;
+                if(flying){
+                    boolean sneaking = player.isSneaking();
+
+                    float speed = 0.08f
+                            * (flying ? 0.6f : 1.0f)
+                            * (sneaking ? 0.1f : 1.0f);
+
+                    if (player.moveForward > 0f) {
+                        player.moveRelative(0f, 0f, 1f, speed);
+                    } else if (player.moveForward < 0f) {
+                        player.moveRelative(0f, 0f, 1f, -speed * 0.3f);
+                    }
+
+                    if (player.moveStrafing != 0f) {
+                        player.moveRelative(1f, 0f, 0f, speed * 0.5f * Math.signum(player.moveStrafing));
+                    }
+                }
+            }else {
+                player.capabilities.allowFlying = false;
+                player.capabilities.isFlying = false;
+            }
         }
     }
 }
