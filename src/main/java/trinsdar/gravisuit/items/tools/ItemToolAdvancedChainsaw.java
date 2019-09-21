219c5d81d8dd235ae9f6b2b1dac1803aa8e7cd52
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-09-21 16:32:46 -0400

    did 2x2 tree chopping

--- src/main/java/trinsdar/gravisuit/items/tools/ItemToolAdvancedChainsaw.java
+++ src/main/java/trinsdar/gravisuit/items/tools/ItemToolAdvancedChainsaw.java
@@ -10,6 +10,7 @@ import ic2.core.platform.textures.Ic2Icons;
 import ic2.core.platform.textures.obj.IStaticTexturedItem;
 import ic2.core.util.misc.StackUtil;
 import ic2.core.util.obj.ToolTipType;
+import net.minecraft.block.BlockLog;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -25,18 +26,25 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.Nullable;
 import trinsdar.gravisuit.util.Config;
 import trinsdar.gravisuit.util.GravisuitLang;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ItemToolAdvancedChainsaw extends ItemElectricTool implements IStaticTexturedItem {
     public static final ItemStack ironAxe;
@@ -121,13 +129,10 @@ public class ItemToolAdvancedChainsaw extends ItemElectricTool implements IStati
     	NBTTagCompound tag = StackUtil.getOrCreateNbtData(itemstack);
         World worldIn = player.world;
         if (!player.isSneaking() && !tag.getBoolean("noTreeCutting")) {
-            for (int i = 1; i < 60; i++) {
-                BlockPos nextPos = pos.up(i);
-                IBlockState nextState = worldIn.getBlockState(nextPos);
-                if (nextState.getBlock().isWood(worldIn, nextPos)) {
-                    breakBlock(nextPos, itemstack, worldIn, pos, player);
-                } else {
-                    break;
+            Set<BlockPos> positions = getTargetBlocks(worldIn, pos, player);
+            if (isTree(pos, player) && !positions.isEmpty()){
+                for (BlockPos pos2 : positions) {
+                    breakBlock(pos2, itemstack, worldIn, pos, player);
                 }
             }
         }
@@ -136,6 +141,169 @@ public class ItemToolAdvancedChainsaw extends ItemElectricTool implements IStati
         } else {
         	return false; 
         }
+    }
+
+    public Set<BlockPos> getTargetBlocks(World worldIn, BlockPos pos, @Nullable EntityPlayer playerIn) {
+        Set<BlockPos> targetBlocks = new HashSet<BlockPos>();
+        if (playerIn == null) {
+            return new HashSet<BlockPos>();
+        }
+        RayTraceResult raytrace = rayTrace(worldIn, playerIn, false);
+        if(raytrace == null || raytrace.sideHit == null){
+            return Collections.emptySet();
+        }
+        EnumFacing enumfacing = raytrace.sideHit;
+        if (enumfacing == EnumFacing.DOWN){
+            return Collections.emptySet();
+        }
+        BlockPos left = left(enumfacing, pos);
+        BlockPos leftback = back(enumfacing, left);
+        BlockPos back = back(enumfacing, pos);
+        BlockPos right = right(enumfacing, pos);
+        BlockPos rightback = back(enumfacing, right);
+        if (enumfacing != EnumFacing.UP && worldIn.getBlockState(back).getBlock().isWood(worldIn, back)){
+            targetBlocks.add(back);
+            for (int i = 1; i < 60; i++) {
+                BlockPos nextPos = back.up(i);
+                IBlockState nextState = worldIn.getBlockState(nextPos);
+                if (nextState.getBlock().isWood(worldIn, nextPos)) {
+                    targetBlocks.add(nextPos);
+                } else {
+                    break;
+                }
+            }
+            if (worldIn.getBlockState(left).getBlock().isWood(worldIn, left) && worldIn.getBlockState(leftback).getBlock().isWood(worldIn, leftback)){
+                targetBlocks.add(left);
+                targetBlocks.add(leftback);
+                for (int i = 1; i < 60; i++) {
+                    BlockPos nextPos = left.up(i);
+                    IBlockState nextState = worldIn.getBlockState(nextPos);
+                    if (nextState.getBlock().isWood(worldIn, nextPos)) {
+                        targetBlocks.add(nextPos);
+                    } else {
+                        break;
+                    }
+                }
+                for (int i = 1; i < 60; i++) {
+                    BlockPos nextPos = leftback.up(i);
+                    IBlockState nextState = worldIn.getBlockState(nextPos);
+                    if (nextState.getBlock().isWood(worldIn, nextPos)) {
+                        targetBlocks.add(nextPos);
+                    } else {
+                        break;
+                    }
+                }
+            } else if (worldIn.getBlockState(right).getBlock().isWood(worldIn, right) && worldIn.getBlockState(rightback).getBlock().isWood(worldIn, rightback)){
+                targetBlocks.add(right);
+                targetBlocks.add(rightback);
+                for (int i = 1; i < 60; i++) {
+                    BlockPos nextPos = right.up(i);
+                    IBlockState nextState = worldIn.getBlockState(nextPos);
+                    if (nextState.getBlock().isWood(worldIn, nextPos)) {
+                        targetBlocks.add(nextPos);
+                    } else {
+                        break;
+                    }
+                }
+                for (int i = 1; i < 60; i++) {
+                    BlockPos nextPos = rightback.up(i);
+                    IBlockState nextState = worldIn.getBlockState(nextPos);
+                    if (nextState.getBlock().isWood(worldIn, nextPos)) {
+                        targetBlocks.add(nextPos);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        
+        if (worldIn.getBlockState(pos).getBlock().isWood(worldIn, pos)){
+            for (int i = 1; i < 60; i++) {
+                BlockPos nextPos = pos.up(i);
+                IBlockState nextState = worldIn.getBlockState(nextPos);
+                if (nextState.getBlock().isWood(worldIn, nextPos)) {
+                    targetBlocks.add(nextPos);
+                } else {
+                    break;
+                }
+            }
+        }
+        return targetBlocks;
+    }
+
+    public BlockPos left(EnumFacing facing, BlockPos old){
+        if (facing == EnumFacing.NORTH){
+            return old.add( 1, 0, 0);
+        }
+        if (facing == EnumFacing.WEST){
+            return old.add( 0, 0, -1);
+        }
+        if (facing == EnumFacing.SOUTH){
+            return old.add( -1, 0, 0);
+        }
+        if (facing == EnumFacing.EAST){
+            return old.add( 0, 0, 1);
+        }
+        return old;
+    }
+
+    public BlockPos right(EnumFacing facing, BlockPos old){
+        if (facing == EnumFacing.NORTH){
+            return old.add( -1, 0, 0);
+        }
+        if (facing == EnumFacing.WEST){
+            return old.add( 0, 0, 1);
+        }
+        if (facing == EnumFacing.SOUTH){
+            return old.add( 1, 0, 0);
+        }
+        if (facing == EnumFacing.EAST){
+            return old.add( 0, 0, -1);
+        }
+        return old;
+    }
+
+    public BlockPos back(EnumFacing facing, BlockPos old){
+        if (facing == EnumFacing.NORTH){
+            return old.add( 0, 0, 1);
+        }
+        if (facing == EnumFacing.WEST){
+            return old.add( 1, 0, 0);
+        }
+        if (facing == EnumFacing.SOUTH){
+            return old.add( 0, 0, -1);
+        }
+        if (facing == EnumFacing.EAST){
+            return old.add( -1, 0, 0);
+        }
+        return old;
+    }
+
+    private boolean shouldBreak(EntityPlayer playerIn, World worldIn, BlockPos originalPos, BlockPos pos) {
+        if (originalPos.equals(pos)) {
+            return false;
+        }
+        IBlockState blockState = worldIn.getBlockState(pos);
+        if (blockState.getMaterial() == Material.AIR) {
+            return false;
+        }
+        if (blockState.getMaterial().isLiquid()) {
+            return false;
+        }
+        float blockHardness = blockState.getPlayerRelativeBlockHardness(playerIn, worldIn, pos);
+        if (blockHardness == -1.0F) {
+            return false;
+        }
+        float originalHardness = worldIn.getBlockState(originalPos).getPlayerRelativeBlockHardness(playerIn, worldIn, originalPos);
+        if ((originalHardness / blockHardness) > 10.0F) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isTree(BlockPos pos, EntityPlayer player){
+        return true;
     }
 
     @Override
