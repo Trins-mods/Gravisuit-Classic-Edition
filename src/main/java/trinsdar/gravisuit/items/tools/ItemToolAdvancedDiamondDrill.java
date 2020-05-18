a60550943d9523261706e6da693e39e4ad01c59b
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-05-17 21:04:23 -0400

    added torch placement to advanced drill

--- src/main/java/trinsdar/gravisuit/items/tools/ItemToolAdvancedDiamondDrill.java
+++ src/main/java/trinsdar/gravisuit/items/tools/ItemToolAdvancedDiamondDrill.java
@@ -11,27 +11,36 @@ import ic2.core.platform.textures.Ic2Icons;
 import ic2.core.platform.textures.obj.IStaticTexturedItem;
 import ic2.core.util.misc.StackUtil;
 import ic2.core.util.obj.ToolTipType;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockShulkerBox;
+import net.minecraft.block.BlockTrapDoor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.Nullable;
@@ -442,6 +451,87 @@ public class ItemToolAdvancedDiamondDrill extends ItemElectricTool implements IS
     @Override
     public boolean canMineBlock(ItemStack d, IBlockState state, IBlockAccess access, BlockPos pos) {
         return ForgeHooks.canToolHarvestBlock(access, pos, d);
+    }
+
+    static boolean gtcxLoaded = Loader.isModLoaded("gtc_expansion");
+    ItemStack torch = new ItemStack(Blocks.TORCH);
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (gtcxLoaded){
+            if (facing != EnumFacing.DOWN){
+                for (ItemStack stack : player.inventory.mainInventory){
+                    if (stack.getItem() == torch.getItem()){
+                        if (placeTorch(player, worldIn, pos, hand, facing, hitX, hitY, hitZ)){
+                            if (!player.isCreative()){
+                                stack.shrink(1);
+                            }
+                            return EnumActionResult.SUCCESS;
+                        }
+                    }
+                }
+            }
+        }
+        return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    public boolean placeTorch(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+        BlockPos offset = pos.offset(facing);
+        if (grass(world, pos)){
+            world.setBlockState(pos, Blocks.TORCH.getDefaultState());
+            world.playSound(null, pos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1.0F, 0.75F);
+            return true;
+        }
+        if (canPlaceAt(world, pos, facing) && world.getBlockState(offset).getBlock() == Blocks.AIR){
+            world.setBlockState(offset, Blocks.TORCH.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, 0, player, hand));
+            world.playSound(null, pos, SoundEvents.BLOCK_WOOD_PLACE, SoundCategory.BLOCKS, 1.0F, 0.75F);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canPlaceAt(World worldIn, BlockPos pos, EnumFacing facing)
+    {
+        BlockPos blockpos = pos.offset(facing.getOpposite());
+        IBlockState state = worldIn.getBlockState(blockpos);
+        Block block = state.getBlock();
+        BlockFaceShape blockfaceshape = state.getBlockFaceShape(worldIn, blockpos, facing);
+
+        if (facing.equals(EnumFacing.UP) && state.getBlock().canPlaceTorchOnTop(state, worldIn, pos))
+        {
+            return true;
+        }
+        else if (facing != EnumFacing.UP && facing != EnumFacing.DOWN)
+        {
+            return !isExceptBlockForAttachWithPiston(block) && blockfaceshape == BlockFaceShape.SOLID;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected static boolean isExceptionBlockForAttaching(Block attachBlock)
+    {
+        return attachBlock instanceof BlockShulkerBox || attachBlock instanceof BlockLeaves || attachBlock instanceof BlockTrapDoor || attachBlock == Blocks.BEACON || attachBlock == Blocks.CAULDRON || attachBlock == Blocks.GLASS || attachBlock == Blocks.GLOWSTONE || attachBlock == Blocks.ICE || attachBlock == Blocks.SEA_LANTERN || attachBlock == Blocks.STAINED_GLASS;
+    }
+
+    protected static boolean isExceptBlockForAttachWithPiston(Block attachBlock)
+    {
+        return isExceptionBlockForAttaching(attachBlock) || attachBlock == Blocks.PISTON || attachBlock == Blocks.STICKY_PISTON || attachBlock == Blocks.PISTON_HEAD;
+    }
+
+
+    public static boolean grass(World world, BlockPos pos) {return grass(block(world, pos), meta(world, pos));}
+    public static boolean grass(Block block, int meta) {
+        if (block == Blocks.TALLGRASS) return true;
+        if (block == Blocks.DOUBLE_PLANT)  return meta ==  2 || meta ==  3;
+        return false;
+    }
+
+    public static Block block(World world, BlockPos pos) {return world.getBlockState(pos).getBlock();}
+    public static int meta(World world, BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+        return state.getBlock().getMetaFromState(state);
     }
 
     public enum ToolMode {
