f96f59112cbaf715a0b2411ca3ba873ed1be6707
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-17 22:18:48 -0400

    mostly finished gravitool

--- src/main/java/trinsdar/gravisuit/items/tools/ItemToolGravitool.java
+++ src/main/java/trinsdar/gravisuit/items/tools/ItemToolGravitool.java
@@ -1,22 +1,37 @@
 package trinsdar.gravisuit.items.tools;
 
 
-import ic2.api.items.electric.ElectricItem;
+import com.google.common.collect.Multimap;
+import ic2.api.crops.ICropModifier;
 import ic2.core.IC2;
 import ic2.core.audio.AudioManager;
 import ic2.core.item.tool.electric.ElectricWrenchTool;
+import ic2.core.platform.player.KeyHelper;
+import ic2.core.platform.registries.IC2Items;
 import ic2.core.utils.helpers.StackUtil;
+import ic2.core.utils.tooltips.ToolTipHelper;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
 import trinsdar.gravisuit.GravisuitClassic;
 import trinsdar.gravisuit.proxy.ClientProxy;
 import trinsdar.gravisuit.util.GravisuitConfig;
@@ -25,11 +40,7 @@ import trinsdar.gravisuit.util.GravisuitSounds;
 import trinsdar.gravisuit.util.Registry;
 import trinsdar.gravisuit.util.RotationHelper;
 
-import java.util.List;
-
-public class ItemToolGravitool extends ElectricWrenchTool {
-
-    //public ModelResourceLocation[] model = new ModelResourceLocation[4];
+public class ItemToolGravitool extends ElectricWrenchTool implements ICropModifier {
 
     public ItemToolGravitool() {
         super("gravitool", null);
@@ -59,17 +70,11 @@ public class ItemToolGravitool extends ElectricWrenchTool {
     public boolean canOverrideLoss(ItemStack stack) {
         return true;
     }
-        
-    //@Override
-    /*public boolean doesSneakBypassUse(ItemStack stack, IBlockAccess world, BlockPos pos, EntityPlayer player) {
-	    return true;
-    }*/
-    /*public int getHarvestLevel(ItemStack stack, String toolClass, @Nullable EntityPlayer player, @Nullable IBlockState blockState) {
-        if (toolClass.equals("wrench") && this.getDamage(stack) > 0){
-            return -1;
-        }
-        return super.getHarvestLevel(stack, toolClass, player, blockState);
-    }*/
+
+    @Override
+    public boolean doesSneakBypassUse(ItemStack stack, LevelReader level, BlockPos pos, Player player) {
+        return true;
+    }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
@@ -96,6 +101,42 @@ public class ItemToolGravitool extends ElectricWrenchTool {
         return super.use(worldIn, playerIn, handIn);
     }
 
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        ItemStack stack = context.getItemInHand();
+        if (!IC2.KEYBOARD.isModeSwitchKeyDown(context.getPlayer())) {
+            if (getMode(stack) == 2) {
+                return IC2Items.ELECTRIC_TREETAP.useOn(context);
+            } else if (getMode(stack) == 1){
+                return IC2Items.ELECTRIC_HOE.useOn(context);
+            }
+        }
+        return super.useOn(context);
+    }
+
+    @Override
+    public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miningEntity) {
+        if (getMode(stack) == 1){
+            return IC2Items.ELECTRIC_HOE.mineBlock(stack, level, state, pos, miningEntity);
+        }
+        return super.mineBlock(stack, level, state, pos, miningEntity);
+    }
+
+    public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
+        if (ToolActions.DEFAULT_HOE_ACTIONS.contains(toolAction)){
+            return getMode(stack) == 1;
+        }
+        return super.canPerformAction(stack, toolAction);
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+        if (getMode(stack) == 1){
+            return IC2Items.ELECTRIC_HOE.getAttributeModifiers(slot, stack);
+        }
+        return super.getAttributeModifiers(slot, stack);
+    }
+
     public byte getMode(ItemStack stack){
         CompoundTag nbt = stack.getOrCreateTag();
         return nbt.getByte("mode");
@@ -103,68 +144,52 @@ public class ItemToolGravitool extends ElectricWrenchTool {
 
     @Override
     public InteractionResult onItemUseFirst(ItemStack stack, UseOnContext context) {
-        if (IC2.PLATFORM.isSimulating() && IC2.KEYBOARD.isModeSwitchKeyDown(context.getPlayer())){
-
+        if (getMode(stack) == 0) {
+            return super.onItemUseFirst(stack, context);
+        } else if (getMode(stack) == 3) {
+            return RotationHelper.rotateBlock(context.getLevel(), context.getClickedPos(), context.getPlayer()!=null && (context.getPlayer().isShiftKeyDown() != context.getClickedFace().equals(Direction.DOWN))) ? InteractionResult.SUCCESS: InteractionResult.PASS;
+        } else {
+            return InteractionResult.PASS;
         }
-        return super.onItemUseFirst(stack, context);
     }
 
-    /*@Override
-    public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
-        ItemStack stack = player.getHeldItem(hand);
-        if (this.getDamage(stack) == 0){
-            return super.onItemUseFirst(player, world, pos, side, hitX, hitY, hitZ, hand);
-        }else if (this.getDamage(stack) == 3){
-            return RotationHelper.rotateBlock(world, pos, side) ? EnumActionResult.SUCCESS: EnumActionResult.PASS;
-        }else {
-            return EnumActionResult.PASS;
+    @Override
+    public void addToolTip(ItemStack stack, Player player, TooltipFlag type, ToolTipHelper helper) {
+        if (this.getMode(stack) == 0){
+            helper.addSimpleToolTip(GravisuitLang.toolMode, Component.translatable(GravisuitLang.wrench));
+        }else if (this.getMode(stack) == 1){
+            helper.addSimpleToolTip(GravisuitLang.toolMode, Component.translatable(GravisuitLang.hoe));
+        }else if (this.getMode(stack) == 2){
+            helper.addSimpleToolTip(GravisuitLang.toolMode, Component.translatable(GravisuitLang.treetap));
+        }else if (this.getMode(stack) == 3){
+            helper.addSimpleToolTip(GravisuitLang.toolMode, Component.translatable(GravisuitLang.screwdriver));
         }
+        if (this.isImport(stack)) {
+            helper.addSimpleToolTip("tooltip.item.ic2.electric_tree_tap.inv_import", new Object[0]);
+        }
+        helper.addKeybindingTooltip(this.buildKeyDescription(KeyHelper.BLOCK_CLICK, "tooltip.item.ic2.hoe.seedmode", new Object[0]));
+        helper.addKeybindingTooltip(this.buildKeyDescription(KeyHelper.MODE_KEY, GravisuitLang.multiModes, new Object[0]));
 
-    }*/
+        /*if (GravisuitConfig.enableGravitoolRequiresLosslessPrecisionWrench && !(Loader.isModLoaded("ic2c_extras") && Ic2cExtrasCodeHelper.isOverridingLossy())){
+            tooltip.add(TextFormatting.GREEN + GravisuitLang.craftingGravitool.getLocalized());
+        }*/
+    }
+
+    @Override
+    public void onLossPrevented(Player player, ItemStack stack) {
+    }
 
     @Override
     public boolean hasBigCost(ItemStack stack) {
         return false;
     }
 
-
-    /*@Override
-    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        ItemStack stack = player.getHeldItem(hand);
-        if (this.getDamage(stack) == 1){
-            return Ic2Items.electricHoe.getItem().onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
-        }else if (this.getDamage(stack) == 2){
-            return Ic2Items.electricTreeTap.getItem().onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
-        }else {
-            return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
-        }
-    }*/
-
-    /*@Override
-    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-        if (this.getDamage(stack) == 0){
-            tooltip.add(GravisuitLang.toolMode.getLocalizedFormatted(GravisuitLang.wrench));
-        }else if (this.getDamage(stack) == 1){
-            tooltip.add(GravisuitLang.toolMode.getLocalizedFormatted(GravisuitLang.hoe));
-        }else if (this.getDamage(stack) == 2){
-            tooltip.add(GravisuitLang.toolMode.getLocalizedFormatted(GravisuitLang.treetap));
-        }else if (this.getDamage(stack) == 3){
-            tooltip.add(GravisuitLang.toolMode.getLocalizedFormatted(GravisuitLang.screwdriver));
-        }
-        if (GravisuitConfig.enableGravitoolRequiresLosslessPrecisionWrench && !(Loader.isModLoaded("ic2c_extras") && Ic2cExtrasCodeHelper.isOverridingLossy())){
-            tooltip.add(TextFormatting.GREEN + GravisuitLang.craftingGravitool.getLocalized());
-        }
-        if (this.isImport(stack)) {
-            tooltip.add(Ic2InfoLang.treeTapEffect.getLocalized());
-        }
-    }*/
-
     public boolean isImport(ItemStack stack) {
         return StackUtil.getNbtData(stack).getBoolean("inv_import");
     }
 
-    /*@Override
-    public boolean isChangingCropDrops(ItemStack itemStack) {
-        return this.getDamage(itemStack) == 1;
-    }*/
+    @Override
+    public boolean canChangeSeedMode(ItemStack itemStack) {
+        return getMode(itemStack) == 1;
+    }
 }
