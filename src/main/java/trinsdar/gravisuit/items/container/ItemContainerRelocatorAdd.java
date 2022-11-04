c0a99569fe9218ce464c7d895864463c1bf7315c
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-11-04 16:50:11 -0400

    made sure to only load components on client side

--- src/main/java/trinsdar/gravisuit/items/container/ItemContainerRelocatorAdd.java
+++ src/main/java/trinsdar/gravisuit/items/container/ItemContainerRelocatorAdd.java
@@ -1,17 +1,22 @@
 package trinsdar.gravisuit.items.container;
 
+import ic2.core.IC2;
 import ic2.core.inventory.container.ItemContainer;
 import ic2.core.inventory.gui.IC2Screen;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import trinsdar.gravisuit.GravisuitClassic;
 
 public class ItemContainerRelocatorAdd extends ItemContainer<ItemInventoryRelocator> {
     public static ResourceLocation TEXTURE = new ResourceLocation(GravisuitClassic.MODID, "textures/gui/relocator_add.png");
     public ItemContainerRelocatorAdd(ItemInventoryRelocator inv, int id, InteractionHand hand, Player player, int windowId) {
         super(inv, player, id, windowId);
-        addComponent(new GuiCompRelocatorAdd(hand, player));
+        if (IC2.PLATFORM.isRendering()) {
+            addComponent(new GuiCompRelocatorAdd(hand, player));
+        }
     }
 
     @Override
@@ -19,6 +24,7 @@ public class ItemContainerRelocatorAdd extends ItemContainer<ItemInventoryReloca
         return TEXTURE;
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Override
     public void onGuiLoaded(IC2Screen screen) {
         super.onGuiLoaded(screen);
--- src/main/java/trinsdar/gravisuit/items/container/ItemContainerRelocatorDisplay.java
+++ src/main/java/trinsdar/gravisuit/items/container/ItemContainerRelocatorDisplay.java
@@ -1,18 +1,23 @@
 package trinsdar.gravisuit.items.container;
 
+import ic2.core.IC2;
 import ic2.core.inventory.container.ItemContainer;
 import ic2.core.inventory.gui.IC2Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import trinsdar.gravisuit.GravisuitClassic;
 
 public class ItemContainerRelocatorDisplay extends ItemContainer<ItemInventoryRelocator> {
     public static ResourceLocation TEXTURE = new ResourceLocation(GravisuitClassic.MODID, "textures/gui/relocator_display.png");
     public ItemContainerRelocatorDisplay(ItemInventoryRelocator inv, int id, InteractionHand hand, Player player, int windowID) {
         super(inv, player, id, windowID);
-        addComponent(new GuiCompRelocatorDisplay(player, hand));
+        if (IC2.PLATFORM.isRendering()) {
+            addComponent(new GuiCompRelocatorDisplay(player, hand));
+        }
     }
 
     @Override
@@ -20,6 +25,7 @@ public class ItemContainerRelocatorDisplay extends ItemContainer<ItemInventoryRe
         return TEXTURE;
     }
 
+    @OnlyIn(Dist.CLIENT)
     @Override
     public void onGuiLoaded(IC2Screen screen) {
         super.onGuiLoaded(screen);
