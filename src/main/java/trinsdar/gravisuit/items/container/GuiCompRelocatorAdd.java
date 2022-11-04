e6305eb0c9428830d2aa49c4e9ab1cd3c28f3d09
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-11-04 13:01:57 -0400

    finished add gui comp

--- src/main/java/trinsdar/gravisuit/items/container/GuiCompRelocatorAdd.java
+++ src/main/java/trinsdar/gravisuit/items/container/GuiCompRelocatorAdd.java
@@ -1,27 +1,23 @@
 package trinsdar.gravisuit.items.container;
 
 import com.google.common.base.Strings;
-import com.mojang.blaze3d.vertex.PoseStack;
-import ic2.core.IC2;
 import ic2.core.inventory.gui.IC2Screen;
 import ic2.core.inventory.gui.components.GuiWidget;
 import ic2.core.utils.helpers.StackUtil;
 import ic2.core.utils.math.geometry.Box2i;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import trinsdar.gravisuit.GravisuitClassic;
 import trinsdar.gravisuit.items.tools.ItemRelocator;
-import trinsdar.gravisuit.network.PacketRelocator;
 import trinsdar.gravisuit.util.GravisuitLang;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 
 public class GuiCompRelocatorAdd extends GuiWidget {
@@ -34,8 +30,8 @@ public class GuiCompRelocatorAdd extends GuiWidget {
     private EditBox textBox;
 
     public GuiCompRelocatorAdd(InteractionHand hand, Player player) {
-        super(new Box2i(21, 18, 1, 11));
-        BOX = new Box2i(21, 18, 18, 11);
+        super(new Box2i(59, 43, 58, 13));
+        BOX = new Box2i(59, 43, 58, 13);
         this.relocator = player.getItemInHand(hand);
         this.hand = hand;
         this.y = 18;
@@ -44,7 +40,7 @@ public class GuiCompRelocatorAdd extends GuiWidget {
 
     @Override
     protected void addRequests(Set<ActionRequest> set) {
-        set.addAll(Arrays.asList(ActionRequest.GUI_INIT, ActionRequest.MOUSE_INPUT, ActionRequest.TOOLTIP, ActionRequest.KEY_INPUT, ActionRequest.DRAW_POST));
+        set.addAll(Arrays.asList(ActionRequest.GUI_INIT, ActionRequest.MOUSE_INPUT, ActionRequest.TOOLTIP, ActionRequest.KEY_INPUT));
     }
 
     @Override
@@ -52,39 +48,64 @@ public class GuiCompRelocatorAdd extends GuiWidget {
         super.init(gui);
         this.textBox = new EditBox(gui.getFont(), bX(gui, 14), bY(gui, 19), 148, 16, Component.empty());
         this.textBox.setMaxLength(32500);
-        this.textBox.setFocus(true);
+        gui.addRenderableWidget(textBox);
+    }
+
+    @Override
+    public boolean isMouseOver(int x, int y) {
+        return super.isMouseOver(x, y);
     }
 
     @Override
     public boolean onMouseClick(int mouseX, int mouseY, int mouseButton) {
-        if (!Strings.isNullOrEmpty(textBox.getValue())){
-            CompoundTag nbt = StackUtil.getNbtData(relocator);
-            int x = (int)player.getX();
-            int y = (int)player.getY();
-            int z = (int)player.getZ();
-            String dimId = player.getLevel().dimension().location().toString();
-            String name = textBox.getValue();
-            ItemRelocator.TeleportData location = new ItemRelocator.TeleportData(x, y, z, dimId, name);
-            CompoundTag map = nbt.getCompound("Locations");
-            boolean successful = false;
-            if (map.size() < 10){
-                successful = true;
+        if (isWithinSetButton(mouseX, mouseY) && mouseButton == 0) {
+            if (!Strings.isNullOrEmpty(textBox.getValue())) {
+                CompoundTag nbt = StackUtil.getNbtData(relocator);
+                int x = (int) player.getX();
+                int y = (int) player.getY();
+                int z = (int) player.getZ();
+                String dimId = player.getLevel().dimension().location().toString();
+                String name = textBox.getValue();
+                ItemRelocator.TeleportData location = new ItemRelocator.TeleportData(x, y, z, dimId, name);
+                CompoundTag map = nbt.getCompound("Locations");
+                boolean successful = false;
+                if (map.size() < 10) {
+                    successful = true;
+                }
+                if (successful) {
+                    //GravisuitClassic.NETWORK.sendToServer(new PacketRelocator(location, PacketRelocator.TeleportFunction.ADDDESTINATION, hand));
+                    player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorAddTeleport, ChatFormatting.GREEN, name), false);
+                } else {
+                    player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorMaxTeleport, ChatFormatting.RED), false);
+                }
+                player.playSound(SoundEvents.UI_BUTTON_CLICK);
+                player.closeContainer();
+                return false;
             }
-            if (successful){
-                //GravisuitClassic.NETWORK.sendToServer(new PacketRelocator(location, PacketRelocator.TeleportFunction.ADDDESTINATION, hand));
-                player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorAddTeleport, ChatFormatting.GREEN, name), false);
-            } else {
-                player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorMaxTeleport, ChatFormatting.RED), false);
-            }
-            player.closeContainer();
-            return false;
-
+            player.playSound(SoundEvents.UI_BUTTON_CLICK);
+        }
+        if (isWithinClearButton(mouseX, mouseY) && mouseButton == 0){
+            this.textBox.setValue("");
+            player.playSound(SoundEvents.UI_BUTTON_CLICK);
         }
         return super.onMouseClick(mouseX, mouseY, mouseButton);
     }
 
     private boolean isWithinSetButton(int mouseX, int mouseY){
-        return mouseX >= bX(gui, 60) && mouseX <= bX(gui, 84) && mouseY >= bY(gui, 44) && mouseY <= bY(gui, 55);
+        return within(mouseX, BOX.getX() + 1, BOX.getX() + 25) && within(mouseY, BOX.getY() + 1, BOX.getY() + 12);
+    }
+
+    private boolean isWithinClearButton(int mouseX, int mouseY){
+        return within(mouseX, BOX.getX() + 33, BOX.getX() + 57) && within(mouseY, BOX.getY() + 1, BOX.getY() + 12);
+    }
+
+    @Override
+    public boolean onKeyTyped(int keyCode) {
+        int keyCompare = gui.getMinecraft().options.keyInventory.getKey().getValue();
+        if (keyCompare == keyCode & textBox.isFocused()) {
+            return true;
+        }
+        return super.onKeyTyped(keyCode);
     }
 
     String name;
@@ -101,23 +122,11 @@ public class GuiCompRelocatorAdd extends GuiWidget {
         }
     }*/
 
-    @Override
-    public boolean onKeyTyped(int keyCode) {
-        //this.textBox.k
-        return super.onKeyTyped(keyCode);
-    }
-
     /*@Override
     public boolean onKeyTyped(GuiIC2 gui, char keyTyped, int keyCode) {
         this.textBox.textboxKeyTyped(keyTyped, keyCode);
         return gui.mc.gameSettings.keyBindInventory.isActiveAndMatches(keyCode);
     }*/
-
-    @Override
-    public void postDraw(PoseStack matrix, int mouseX, int mouseY, float partialTicks) {
-        super.postDraw(matrix, mouseX, mouseY, partialTicks);
-        this.textBox.render(matrix, mouseX, mouseY, partialTicks);
-    }
 
     private int bX(IC2Screen gui, int position) {
         return gui.getGuiLeft() + position;
