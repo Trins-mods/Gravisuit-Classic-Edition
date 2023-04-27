207d8a6869c93dc3d794c6e5a55d894a5a08226a
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-27 00:51:37 -0400

    added ticker to plasma portal

--- src/main/java/trinsdar/gravisuit/block/BlockEntityPlasmaPortal.java
+++ src/main/java/trinsdar/gravisuit/block/BlockEntityPlasmaPortal.java
@@ -1,15 +1,30 @@
 package trinsdar.gravisuit.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import trinsdar.gravisuit.util.Registry;
 
 public class BlockEntityPlasmaPortal extends BlockEntity {
+    int ticker = 0;
     public BlockEntityPlasmaPortal(BlockPos arg2, BlockState arg3) {
         super(Registry.PLASMA_PORTAL_BLOCK_ENTITY, arg2, arg3);
     }
 
+    public static void tick(Level level, BlockPos pos, BlockState state, BlockEntity blockEntity){
+        if (blockEntity instanceof BlockEntityPlasmaPortal portal) {
+            portal.onTick(level, pos, state);
+        }
+    }
 
+    public void onTick(Level level, BlockPos pos, BlockState state){
+        ticker++;
+        if (ticker >= 40){
+            level.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
+        }
+    }
 }
--- src/main/java/trinsdar/gravisuit/block/BlockPlasmaPortal.java
+++ src/main/java/trinsdar/gravisuit/block/BlockPlasmaPortal.java
@@ -2,9 +2,12 @@ package trinsdar.gravisuit.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -27,5 +30,11 @@ public class BlockPlasmaPortal extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return Registry.PLASMA_PORTAL_BLOCK_ENTITY.create(pos, state);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
+        return BlockEntityPlasmaPortal::tick;
     }
 }
