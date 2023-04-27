ef9f9cb51ae813f169984c1667da161e785576c9
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-04-27 00:51:25 -0400

    made plasma portal have no bounding box

--- src/main/java/trinsdar/gravisuit/block/BlockPlasmaPortal.java
+++ src/main/java/trinsdar/gravisuit/block/BlockPlasmaPortal.java
@@ -1,17 +1,26 @@
 package trinsdar.gravisuit.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 import trinsdar.gravisuit.util.Registry;
 
 public class BlockPlasmaPortal extends Block implements EntityBlock {
     public BlockPlasmaPortal() {
         super(Properties.of(Material.PORTAL).instabreak().noCollission().noOcclusion().noLootTable());
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.empty();
     }
 
     @Nullable
