7190eebcc3669aa42eea36e5f670a0fd61f77e8b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-09-05 21:26:31 -0400

    added tooltips to relocator
    also improved code for getting mode in ItemRelocator

--- src/main/java/trinsdar/gravisuit/items/tools/ItemRelocator.java
+++ src/main/java/trinsdar/gravisuit/items/tools/ItemRelocator.java
@@ -1,5 +1,6 @@
 package trinsdar.gravisuit.items.tools;
 
+import com.google.common.base.CaseFormat;
 import ic2.api.items.electric.ElectricItem;
 import ic2.api.tiles.teleporter.TeleporterTarget;
 import ic2.core.IC2;
@@ -7,14 +8,18 @@ import ic2.core.audio.AudioManager;
 import ic2.core.inventory.base.IHasHeldGui;
 import ic2.core.inventory.base.IPortableInventory;
 import ic2.core.item.base.IC2ElectricItem;
+import ic2.core.platform.player.KeyHelper;
 import ic2.core.platform.rendering.IC2Textures;
 import ic2.core.platform.rendering.features.item.ISimpleItemModel;
+import ic2.core.utils.helpers.StackUtil;
 import ic2.core.utils.helpers.TeleportUtil;
+import ic2.core.utils.tooltips.ToolTipHelper;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
@@ -22,6 +27,7 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -70,34 +76,35 @@ public class ItemRelocator extends IC2ElectricItem implements ISimpleItemModel, 
     }
 
     @Override
+    public void addToolTip(ItemStack stack, Player player, TooltipFlag type, ToolTipHelper helper) {
+        super.addToolTip(stack, player, type, helper);
+        TeleportMode mode = getTeleportMode(stack);
+        helper.addSimpleToolTip(Component.translatable("item_info.toolMode", Component.translatable(mode.localeName).withStyle(mode.color)).withStyle(ChatFormatting.BLUE));
+        helper.addKeybindingTooltip(this.buildKeyDescription(KeyHelper.MODE_KEY, KeyHelper.RIGHT_CLICK, Component.translatable("item_info.multiModes").withStyle(ChatFormatting.GRAY)));
+    }
+
+    @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
         CompoundTag nbt = stack.getOrCreateTag();
         if (IC2.PLATFORM.isSimulating() && IC2.KEYBOARD.isModeSwitchKeyDown(player)){
-            byte mode = nbt.getByte("mode");
-            if (mode == 2) {
-                nbt.putByte("mode", (byte) 0);
-                player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorPersonal, ChatFormatting.GREEN), false);
-            } else if (mode == 0){
-                nbt.putByte("mode", (byte) 1);
-                player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorTranslocator, ChatFormatting.GOLD), false);
-            } else {
-                nbt.putByte("mode", (byte) 2);
-                player.displayClientMessage(this.translate(GravisuitLang.messageRelocatorPortal, ChatFormatting.AQUA), false);
-            }
+            TeleportMode nextMode = getNextTeleportMode(stack);
+            saveTeleportMode(stack, nextMode);
+            player.displayClientMessage(Component.translatable("message.relocator", Component.translatable(nextMode.localeName).withStyle(nextMode.color)).withStyle(ChatFormatting.YELLOW), false);
             return InteractionResultHolder.success(stack);
         }
-        if (IC2.PLATFORM.isSimulating())
-            if (player.isCrouching() || nbt.getByte("mode") == 0) {
+        if (IC2.PLATFORM.isSimulating()) {
+            TeleportMode mode = getTeleportMode(stack);
+            if (player.isCrouching() || mode == TeleportMode.PERSONAL) {
                 IC2.PLATFORM.launchGui(player, hand, null, this.getInventory(player, hand, stack));
                 return InteractionResultHolder.success(stack);
-            } else if (nbt.getByte("mode") >= 1){
+            } else {
                 if (nbt.contains("DefaultLocation")){
                     if (nbt.contains("Locations")){
                         CompoundTag map = nbt.getCompound("Locations");
                         String name = nbt.getString("DefaultLocation");
                         if (map.contains(name)){
-                            boolean portal = nbt.getByte("mode") == 2;
+                            boolean portal = mode == TeleportMode.PORTAL;
                             int use = portal ? 10000000 : 500000;
                             if (ElectricItem.MANAGER.canUse(stack, use)) {
                                 PlasmaBall entity = new PlasmaBall(player.level, player, TeleportData.fromNBT(map.getCompound(name), name), hand);
@@ -113,6 +120,7 @@ public class ItemRelocator extends IC2ElectricItem implements ISimpleItemModel, 
                 }
 
             }
+        }
         return super.use(level, player, hand);
     }
 
@@ -141,17 +149,41 @@ public class ItemRelocator extends IC2ElectricItem implements ISimpleItemModel, 
     }
 
     public enum TeleportMode {
-        PERSONAL,
-        PORTAL,
-        TRANSLOCATOR;
+        PERSONAL(ChatFormatting.GREEN),
+        TRANSLOCATOR(ChatFormatting.GOLD),
+        PORTAL(ChatFormatting.AQUA);
 
-        public TeleportMode getNext() {
-            return switch (this){
-                case PERSONAL -> PORTAL;
-                case PORTAL -> TRANSLOCATOR;
-                case TRANSLOCATOR -> PERSONAL;
-            };
+
+        private static final TeleportMode[] VALUES = values();
+        public final ChatFormatting color;
+        public final String name;
+        public final String localeName;
+
+        TeleportMode(ChatFormatting color) {
+            this.name = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_UNDERSCORE, name());
+            this.localeName = "message.text.mode." + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_UNDERSCORE, name());
+            this.color = color;
         }
+
+        public static TeleportMode getFromId(int ID) {
+            return VALUES[ID % VALUES.length];
+        }
+
+    }
+
+    public static TeleportMode getTeleportMode(ItemStack tool) {
+        CompoundTag tag = StackUtil.getNbtData(tool);
+        return TeleportMode.getFromId(tag.getByte("mode"));
+    }
+
+    public static TeleportMode getNextTeleportMode(ItemStack tool) {
+        CompoundTag tag = StackUtil.getNbtData(tool);
+        return TeleportMode.getFromId(tag.getByte("mode") + 1);
+    }
+
+    public static void saveTeleportMode(ItemStack tool, TeleportMode mode) {
+        CompoundTag tag = StackUtil.getNbtData(tool);
+        tag.putByte("mode", (byte) mode.ordinal());
     }
 
     public static class TeleportData {
